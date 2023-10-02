29f83f339666426cbf4bfb2755591c8acf951428
matthewcroughan <matt@croughan.sh>
2023-10-02 23:09:51 +0100

    base: specify nix.settings and boot.loader explicitly in base

--- base.nix
+++ base.nix
@@ -1,7 +1,5 @@
 { pkgs, config, lib, ... }:
 {
-  # Use latest kernel
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
   # This causes an overlay which causes a lot of rebuilding
   environment.noXlibs = lib.mkForce false;
   # "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix" creates a
@@ -11,4 +9,15 @@
     { device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
     };
+  boot = {
+    kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+    loader = {
+      generic-extlinux-compatible.enable = lib.mkDefault true;
+      grub.enable = lib.mkDefault false;
+    };
+  };
+  nix.settings = {
+    experimental-features = lib.mkDefault "nix-command flakes";
+    trusted-users = [ "root" "@wheel" ];
+  };
 }
