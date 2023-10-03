937af7e255d2f8ccd3c23deb3321e2c6f4873dc7
Robert James Hernandez <rob@sarcasticadmin.com>
2023-10-03 19:36:45 +0000

    flash eeprom: setting the flash as optional
    From my understanding about the firmware and how the raspberry pi
    detects flashing, bootconfig, etc. it seems like the file actually has
    to be named differently to signal uboot behavior.
    
    I still need to confirm this but including what I have up to this point.

--- flake.nix
+++ flake.nix
@@ -26,64 +26,67 @@
             pi-eeprom-sdImage = (self.nixosConfigurations.pi-eeprom.extendModules {
               modules = [ "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-new-kernel-no-zfs-installer.nix" ];
             }).config.system.build.sdImage;
-            build-eeprom =
-              let
-                bootconf = pkgs.writeText "bootconf" ''
-                  [all]
-                  BOOT_UART=1
-                  WAKE_ON_GPIO=1
-                  POWER_OFF_ON_HALT=0
-                  ENABLE_SELF_UPDATE=1
-                  BOOT_ORDER=0xf2 # 2=Network boot 1=sdcard, order matters
-                  NET_BOOT_MAX_RETRIES=5
-                  USE_IPV6=1 # Enable IPv6, netboot is on by default
+            build-eeprom = lib.mkOverridable
+              { flash ? true }:
+                let
+                  bootconf = pkgs.writeText "bootconf" ''
+                    [all]
+                    BOOT_UART=1
+                    WAKE_ON_GPIO=1
+                    POWER_OFF_ON_HALT=0
+                    ENABLE_SELF_UPDATE=1
+                    BOOT_ORDER=0xf2 # 2=Network boot 1=sdcard, order matters
+                    NET_BOOT_MAX_RETRIES=5
+                    USE_IPV6=1 # Enable IPv6, netboot is on by default
 
-                  [none]
-                  FREEZE_VERSION=0
-                '';
-                pkgs = nixpkgs.legacyPackages.aarch64-linux;
-              in
-              pkgs.runCommand "eeprom.img"
-                {
-                  buildInputs = [ pkgs.raspberrypi-eeprom ];
-                } ''
-                mkdir $out
-                # Apply config
-                echo ${bootconf}
-                rpi-eeprom-config --config ${bootconf} --out $out/pieeprom.upd ${pkgs.raspberrypi-eeprom}/share/rpi-eeprom/latest/pieeprom-2023-01-11.bin
-                sha256sum $out/pieeprom.upd | cut -d' ' -f1 > $out/pieeprom.sig
-                cp ${pkgs.raspberrypi-eeprom}/share/rpi-eeprom/latest/recovery.bin $out/recovery.bin
-                # This needs to increase after every update to the eeprom
-                echo 'ts: 102' >> $out/pieeprom.sig
-              '';
+                    [none]
+                    FREEZE_VERSION=0
+                  '';
+                  pkgs = nixpkgs.legacyPackages.aarch64-linux;
+                  pieeprom = if flash then "pieeprom.bin" else "pieeprom.upd";
+                in
+                pkgs.runCommand "eeprom.img"
+                  {
+                    buildInputs = [ pkgs.raspberrypi-eeprom ];
+                  } ''
+                    mkdir $out
+                    # Apply config
+                    echo ${bootconf}
+                    rpi-eeprom-config --config ${bootconf} --out $out/${pieeprom} ${pkgs.raspberrypi-eeprom}/share/rpi-eeprom/latest/pieeprom-2023-01-11.bin
+                    sha256sum $out/${pieeprom} | cut -d' ' -f1 > $out/pieeprom.sig
+                    cp ${pkgs.raspberrypi-eeprom}/share/rpi-eeprom/latest/recovery.bin $out/recovery.bin
+                    # This needs to increase after every update to the eeprom
+                    echo 'ts: 102' >> $out/pieeprom.sig
+                  '';
+              );
+            };
           };
-        };
-        nixosConfigurations = {
-          pi-kiosk = nixpkgs.lib.nixosSystem {
-            system = "aarch64-linux";
-            modules = [
-              nixos-hardware.nixosModules.raspberry-pi-4
-              ./configuration.nix
-              ./base.nix
-            ];
-          };
-          pi-eeprom = nixpkgs.lib.nixosSystem {
-            system = "aarch64-linux";
-            modules = [
-              nixos-hardware.nixosModules.raspberry-pi-4
-              ./base.nix
-              ({ pkgs, ... }: {
-                environment.systemPackages = with pkgs; [ raspberrypi-eeprom ];
-                # without this we dont actually get a shell
-                boot.kernelParams = [
-                  "console=ttyS0,115200"
-                  "console=tty1"
-                ];
-              })
-            ];
-          };
+          nixosConfigurations = {
+            pi-kiosk = nixpkgs.lib.nixosSystem {
+              system = "aarch64-linux";
+              modules = [
+                nixos-hardware.nixosModules.raspberry-pi-4
+                ./configuration.nix
+                ./base.nix
+              ];
+            };
+            pi-eeprom = nixpkgs.lib.nixosSystem {
+              system = "aarch64-linux";
+              modules = [
+                nixos-hardware.nixosModules.raspberry-pi-4
+                ./base.nix
+                ({ pkgs, ... }: {
+                  environment.systemPackages = with pkgs; [ raspberrypi-eeprom ];
+                  # without this we dont actually get a shell
+                  boot.kernelParams = [
+                    "console=ttyS0,115200"
+                    "console=tty1"
+                  ];
+                })
+              ];
+            };
 
+          };
         };
       };
-    };
-}
+    }
