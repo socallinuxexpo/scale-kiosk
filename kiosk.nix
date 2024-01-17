360d533b1acee96af75f15a65a308e4849aa4cca
Robert James Hernandez <rob@sarcasticadmin.com>
2024-01-17 08:37:11 +0000

    polkit

--- kiosk.nix
+++ kiosk.nix
@@ -18,4 +18,18 @@ in
     user = "kiosk";
     program = kioskProgram;
   };
+
+  # Whitelist wheel users to do anything
+  # This is useful for things like pkexec
+  #
+  # WARNING: this is dangerous for systems
+  # outside the installation-cd and shouldn't
+  # be used anywhere else.
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (subject.isInGroup("wheel")) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
 }
