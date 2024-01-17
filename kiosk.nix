a19276e417d5c5e574c07754fb86c17bcc24c58f
Robert James Hernandez <rob@sarcasticadmin.com>
2024-01-17 18:27:07 +0000

    kiosk: chromium exec options in cage
    Chromium needed to be able to write to the CWD. Similar issue was
    reported:
    https://github.com/NixOS/nixpkgs/issues/229235#issuecomment-1891060190
    
    reg url is an iframe and needs to have some of the more secure options
    in chromium disabled.
    
    Disabling the GPU for good measure since we were seeing errors in the
    logs related to rpi GPU and wayland.

--- kiosk.nix
+++ kiosk.nix
@@ -1,14 +1,20 @@
 { pkgs, lib, ... }:
 let
-  mouseUrl = "https://google.com";
-  regularUrl = "https://duckduckgo.com";
+  mouseUrl = "https://register.socallinuxexpo.org/reg6/kiosk/";
+  regularUrl = "http://signs.scale.lan/";
   kioskProgram = pkgs.writeShellScript "kiosk.sh" ''
-    if [ -e /sys/class/input/mouse0 ]
-    then
-      ${lib.getExe pkgs.chromium} --disable-infobars --start-maximized --kiosk ${mouseUrl}
-    else
-      ${lib.getExe pkgs.chromium} --disable-infobars --start-maximized --kiosk ${regularUrl}
-    fi
+    cd /home/kiosk
+    # account for ALT+F4 closing window in wayland
+    while true
+    do
+      if [ -e /sys/class/input/mouse0 ]
+      then
+        # required cross-origin-iframe and popup blocking flags due to iframe
+        ${lib.getExe pkgs.chromium} --ozone-platform=wayland --disable-popup-blocking --disable-throttle-non-visible-cross-origin-iframes --incognito --start-maximized --disable-gpu --kiosk ${mouseUrl}
+      else
+        ${lib.getExe pkgs.chromium} --ozone-platform=wayland --incognito --start-maximized --disable-gpu --kiosk ${regularUrl}
+      fi
+    done
   '';
 in
 {
