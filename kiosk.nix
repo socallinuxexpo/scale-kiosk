9c1d362fd26b12d4264da62de672798dc1d05bdb
Robert James Hernandez <rob@sarcasticadmin.com>
2024-01-17 18:42:06 +0000

    chromium: updates URLs for Scale

--- kiosk.nix
+++ kiosk.nix
@@ -1,14 +1,14 @@
 { pkgs, lib, ... }:
 let
-  mouseUrl = "https://google.com";
-  regularUrl = "https://duckduckgo.com";
+  mouseUrl = "https://register.socallinuxexpo.org/reg6/kiosk/";
+  regularUrl = "http://signs.scale.lan/";
   kioskProgram = pkgs.writeShellScript "kiosk.sh" ''
     cd /home/kiosk
     if [ -e /sys/class/input/mouse0 ]
     then
-      ${lib.getExe pkgs.chromium} --ozone-platform=wayland --start-maximized --kiosk ${mouseUrl}
+      ${lib.getExe pkgs.chromium} --ozone-platform=wayland --incognito --start-maximized --kiosk ${mouseUrl}
     else
-      ${lib.getExe pkgs.chromium} --ozone-platform=wayland --start-maximized --kiosk ${regularUrl}
+      ${lib.getExe pkgs.chromium} --ozone-platform=wayland --incognito --start-maximized --kiosk ${regularUrl}
     fi
   '';
 in
