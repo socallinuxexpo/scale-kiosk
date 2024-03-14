0e7f966ce1fb0e144eaca3007a5ba827d8e2f1d8
matthewcroughan <matt@croughan.sh>
2024-03-14 08:23:01 +0000

    use ungoogled-chromium instead of chromium

--- kiosk.nix
+++ kiosk.nix
@@ -2,6 +2,8 @@
 let
   mouseUrl = "https://register.socallinuxexpo.org/reg6/?kiosk=1";
   regularUrl = "http://signs.scale.lan/";
+  # using electron should be preferable, but in practice it has some
+  # quirks that make using chromium better
   kioskProgram = pkgs.writeShellScript "kiosk.sh" ''
     LAST_OCTET=$(ip -j route | ${lib.getExe pkgs.jq} '.[] | select(.dst == "default") | .prefsrc | split(".") | .[-1]' -r)
     cd /home/kiosk
@@ -11,9 +13,9 @@ let
       if [ -e /sys/class/input/mouse1 ]
       then
         # required cross-origin-iframe and popup blocking flags due to iframe
-        ${lib.getExe pkgs.chromium} --blink-settings=allowScriptsToCloseWindows=true --ozone-platform=wayland --user-agent="SCALE:$LAST_OCTET" --disable-popup-blocking --disable-throttle-non-visible-cross-origin-iframes --incognito --start-maximized --disable-gpu --kiosk ${mouseUrl}
+        ${lib.getExe pkgs.ungoogled-chromium} --blink-settings=allowScriptsToCloseWindows=true --ozone-platform=wayland --user-agent="SCALE:$LAST_OCTET" --disable-popup-blocking --disable-throttle-non-visible-cross-origin-iframes --incognito --start-maximized --disable-gpu --kiosk ${mouseUrl}
       else
-        ${lib.getExe pkgs.chromium} --ozone-platform=wayland --incognito --start-maximized --disable-gpu --kiosk ${regularUrl}
+        ${lib.getExe pkgs.ungoogled-chromium} --ozone-platform=wayland --incognito --start-maximized --disable-gpu --kiosk ${regularUrl}
       fi
     done
   '';
