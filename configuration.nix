283414ddf199510f3733e04d671a53469c3c3a5e
kylerisse <kylerisse@users.noreply.github.com>
2026-02-17 18:31:12 -0800

    users.root: rotate password

--- configuration.nix
+++ configuration.nix
@@ -68,7 +68,7 @@
   };
 
   users.mutableUsers = false;
-  users.extraUsers.root.hashedPassword = "$6$3Hm/K5fbR3UEMK6H$3aaegtdwvejGk9Bk0ttN5bNJn4z2Yt6LWXD3nGI7.44Pbm7A1TpKuxG9XQLwsj7M9NEk8eB5Exg0qVRV//6br/";
+  users.extraUsers.root.hashedPassword = "$6$nixtheplanet$7rjv9t572/PWzMmsGvuTEcVhghAuJ91s.8Bc0Kli4PuEoMupqoyQGq7qPmdlpui.Q1l9yjA5UzsMWJgTkhMbf1";
 
   nix.settings = {
     experimental-features = lib.mkDefault "nix-command flakes";
